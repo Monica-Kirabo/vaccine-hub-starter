f083079237cff84077fd83c50d16fce61b1eeea5
monica kirabo <kirabom@duq.edu>
2022-07-07 00:58:23 -0700

    commit here

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -4,7 +4,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app=express()
 const nodemon=require("nodemon")
+
 const {BadReqeustError, NotFoundError}=require("./utils/errors")
+const {PORT}=require("../config")
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -13,7 +15,7 @@ app.use((err,res,next)=>{
     return next(new NotFoundError)
 })
 
-const PORT=process.env.PORT||3001
+//const PORT=process.env.PORT||3001
 
 app.listen(PORT,()=>{
     console.log(`🚀 Server listening on port ` + port)
