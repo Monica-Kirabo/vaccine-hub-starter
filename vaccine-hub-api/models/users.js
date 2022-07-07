dff39a9b59770d0b7af289479c639be7099dacae
monica kirabo <kirabom@duq.edu>
2022-07-07 02:00:58 -0700

    commit here

--- vaccine-hub-api/models/users.js
+++ vaccine-hub-api/models/users.js
@@ -1,14 +1,41 @@
-const {unauthorizedError}=require("../utils/errors")
+const { unauthorizedError, BadRequestError}=require("../utils/errors")
 const db=require("../db")
+const { useScrollTrigger } = require("@material-ui/core")
 class User{
     static async login(credentials){
         //user should submit their email and password
         //if any of these fields are missing, throw an error
      throw new unauthorizedError("Invalid email/password")
     }
+
     static async register(credentials){
-     //user should submit email, pw
+  const requiredFields=["email","password","rsvpstatus","numGuests"]
+  requiredFields.forEach(field=>{
+    if(!credentials.hasOwnProperty(field)){
+        throw new BadRequestError('Missing ${field} in request body.')
+
     }
+  })
+  const existingUser=await user.fetchUserByEmail(credentials.email)
+  if(existingUser){
+    throw new BadRequestError('Duplicate email: ${credentials.email}')
+  }
+const lowercaseEmail=credentials.email.toLowerCase()
+const result=await db.query(' INSERT INTO users(email,password,rsvp_status,num_guests,created_at) VALUES($1,$2,$3,$4) RETURNING id,email,rsvp_status,num_guests,created_at ',[lowercaseEmail,credentials.password,credentials.rsvpstatus,credentials.numGuests])
+const user=result.rows[0]
+return user
+
+    }
+
+    static async fetchUserByEmail(email){
+        if(!email){
+            throw new BadRequestError("No email provided")
+        }
+        const query='SELECT * FROM users WHERE email=$1'
+        const result=await db.query(query,[email.toLowerCase()])
+        const user=result.rows[0]
+        return user
+       }
 }
 
 module.exports=User
--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const user=require("../models/users")
 router.post("/login", async (req, res, next) => {
   try {
+    const user=await user.login(req.body)
+    return res.status(200).json({user})
   } catch (err) {
     next(err);
   }
@@ -10,6 +12,8 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
   try {
+    const user=await user.register(req.body)
+    return res.status(201).json({user})
   } catch (err) {
     next(err);
   }
