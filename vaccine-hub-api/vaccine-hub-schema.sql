01ced807d55daf2aa4a25379322c52f0e2aa432d
monica kirabo <kirabom@duq.edu>
2022-07-07 00:09:19 -0700

    commit here

---
+++ vaccine-hub-api/vaccine-hub-schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    location TEXT NOT NULL,
+    date TEXT NOT NULL,
+
+
+    email TEXT NOT NULL 
+
+);
