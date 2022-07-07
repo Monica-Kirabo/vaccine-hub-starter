62494d463ae8ed2a8a923aa5d8dba618cf873c18
monica kirabo <kirabom@duq.edu>
2022-07-07 11:14:10 -0700

    commit here

--- vaccine-hub-api/db.js
+++ vaccine-hub-api/db.js
@@ -1,13 +1,14 @@
-const {Client}=require("pg")
-const {getDatabaseUri}=require("../config")
-require("colors")
-const db=new Client({connectionString:getDatabaseUri})
-db.connect((err)=>{
-    if(err){
-        console.error("connection error".red, err.stack)
-    }else{
-        console.log("successfully connected to postgres db!".blue)
-    }
-})
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+require("colors");
+const db = new Client({ connectionString: getDatabaseUri() });
+// console.log(getDatabaseUri);
+db.connect((err) => {
+  if (err) {
+    console.error("connection error".red, err.stack);
+  } else {
+    console.log("successfully connected to postgres db!".blue);
+  }
+});
 
-module.exports=db
+module.exports = db;
--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,24 +1,23 @@
-
 const express = require("express");
 const morgan = require("morgan");
-const auth=require("./routes/auth")
+const auth = require("./routes/auth");
 const cors = require("cors");
-const app=express()
-const nodemon=require("nodemon")
+const app = express();
+const nodemon = require("nodemon");
 
-const {BadReqeustError, NotFoundError}=require("./utils/errors")
-const {PORT}=require("../config")
+const { BadReqeustError, NotFoundError } = require("./utils/errors");
+const { PORT } = require("./config");
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(nodemon())
-app.use("/auth",auth)
-app.use((err,res,next)=>{
-    return next(new NotFoundError)
-})
+// app.use(nodemon());
+app.use("/auth", auth);
+app.use((err, res, next) => {
+  return next(new NotFoundError());
+});
 
-//const PORT=process.env.PORT||3001
+const port = process.env.PORT || 3001;
 
-app.listen(PORT,()=>{
-    console.log(`🚀 Server listening on port ` + port)
-})
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on port ` + port);
+});
